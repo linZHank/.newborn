4501277e1294fc4cdff8efef493d57a383088ef9
linzhanguca <lzhang12@uca.edu>
2022-03-09 12:44:58 -0600

    ubuntu branch

--- setup.sh
+++ setup.sh
@@ -1,21 +1,22 @@
-# install nix
-sh <(curl -L https://nixos.org/nix/install) --daemon
+# download packages info and upgrade packages
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update && sudo apt upgrade
 
 # install packages
-nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.git \
-	nixpkgs.neovim \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.bat \
-	nixpkgs.gnumake \
-	nixpkgs.gcc \
+sudo apt install \
+    build-essential \
+    zsh \
+    git \
+    neovim \
+    tmux \
+    stow \
 
 # stow dotfiles
-stow git
-stow nvim
+stow x
 stow zsh
+stow nvim
+stow git
+stow tmux
 
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
@@ -23,4 +24,14 @@ command -v zsh | sudo tee -a /etc/shells
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
 
-# TODO: install nerd-fonts
+# install nerd-fonts
+mkdir -p $HOME/.local/share/fonts/NerdFonts/
+cd $HOME/.local/share/fonts/NerdFonts/
+curl -fLo "Hack BoldItalic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
+curl -fLo "Hack BoldItalic Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "Hack Bold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf
+curl -fLo "Hack Bold Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "Hack Italic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete.ttf
+curl -fLo "Hack Italic Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
+curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
