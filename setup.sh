f09e11a97af900383d6625c75fc9f61d9837bac1
Lin Zhang <linzhank@gmail.com>
2022-06-06 22:37:33 -0500

    update setup script

--- setup.sh
+++ setup.sh
@@ -43,26 +43,9 @@ curl -fLo "Hack Italic Nerd Font Complete Mono.ttf" https://github.com/ryanoasis
 curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
 curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 
-# install ros-galactic, comment next block if ros not wanted
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update
-sudo apt install -y ros-galactic-ros-base python3-colcon-common-extensions
+# install ros-humble, comment next block if ros not wanted
 
 # install nvidia driver and cuda
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-sudo apt update
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/x86_64/nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
-sudo apt install ./nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
-sudo apt update
-wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/x86_64/libnccl2_2.8.3-1+cuda11.2_amd64.deb
-sudo apt install ./libnccl2_2.8.3-1+cuda11.2_amd64.deb
-sudo apt update
-sudo apt install --no-install-recommends cuda-11-0 libcudnn8 libcudnn8-dev
-sudo apt install -y --no-install-recommends libnvinfer8 libnvinfer-dev libnvinfer-plugin8
 
 # install node version manager
 curl -fsSL https://fnm.vercel.app/install | bash
