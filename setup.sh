1718d48addff028308433f6dc87ab72873bb07db
Lin Zhang <linzhank@gmail.com>
2022-06-08 14:04:09 -0500

    add ROS Humble installation and config

--- setup.sh
+++ setup.sh
@@ -15,11 +15,7 @@ sudo apt install -y \
     tmux \
     stow \
     python3-dev \
-    python3-pip \
-
-# install nvim
-wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
-sudo apt install ./nvim-linux64.deb
+    python3-pip
 
 # install pyenv
 sudo apt install -y \
@@ -36,8 +32,41 @@ sudo apt install -y \
     libxml2-dev \
     libxmlsec1-dev \
     libffi-dev \
-    liblzma-dev \
+    liblzma-dev
+
 curl https://pyenv.run | bash
+
+# install ros-humble, comment next block if ros not wanted
+sudo apt install -y gnupg lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update
+sudo apt install -y ros-humble-desktop python3-colcon-common-extensions
+
+# install node version manager
+curl -fsSL https://fnm.vercel.app/install | bash
+export PATH=$HOME/.fnm:$PATH
+eval "$(fnm env --use-on-cd)"
+fnm install v16.14.0
+
+# install linting, formatting tools
+pip install pip pyright flake8 black --upgrade # python
+sudo apt install -y libxml2-utils tidy  # xml
+npm install -g markdownlint-cli  # markdown
+
+# install nvidia driver 
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
+sudo apt update
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
+sudo apt install -y ./nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
+
+# install nvim
+wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+sudo apt remove neovim* --purge
+sudo apt install -y ./nvim-linux64.deb
 
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
@@ -46,6 +75,7 @@ command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER
 
 # stow dotfiles
+cd $HOME/.newborn
 stow alacritty
 stow zsh
 stow nvim
@@ -64,36 +94,10 @@ curl -fLo "Hack Italic Nerd Font Complete Mono.ttf" https://github.com/ryanoasis
 curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
 curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 
-# install ros-humble, comment next block if ros not wanted
-
-# install node version manager
-curl -fsSL https://fnm.vercel.app/install | bash
-export PATH=$HOME/.fnm:$PATH
-eval "$(fnm env --use-on-cd)"
-fnm install v16.14.0
-
-# install linting, formatting tools
-pip install pip pyright flake8 black --upgrade # python
-sudo apt install libxml2-utils tidy  # xml
-npm install -g markdownlint-cli  # markdown
-
-# install nvidia driver and cuda
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
-sudo apt update
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2204/x86_64/nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
-sudo apt install ./nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
-
-# install conda, cuda and tf
+# install conda
+cd $HOME
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh
-conda create --name tensorflow python=3.9
-conda activate tf
-conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
-pip install tensorflow
-python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
 
 # restart computer
 echo "you may now restart your computer"
