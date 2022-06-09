74ef3e0dc292f3357324a7fd6a9e25f41e48b32c
linZHank <linzhank@gmail.com>
2022-06-09 15:09:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,15 +33,12 @@ sudo apt install -y \
     libxmlsec1-dev \
     libffi-dev \
     liblzma-dev
-
 curl https://pyenv.run | bash
 
-# install ros-humble, comment next block if ros not wanted
-sudo apt install -y gnupg lsb-release
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update
-sudo apt install -y ros-humble-desktop python3-colcon-common-extensions
+# install nvim
+wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+sudo apt remove neovim* --purge
+sudo apt install -y ./nvim-linux64.deb
 
 # install node version manager
 curl -fsSL https://fnm.vercel.app/install | bash
@@ -54,25 +51,20 @@ pip install pip pyright flake8 black --upgrade # python
 sudo apt install -y libxml2-utils tidy  # xml
 npm install -g markdownlint-cli  # markdown
 
-# install nvidia driver 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
-sudo apt update
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
-sudo apt install -y ./nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
-
-# install nvim
-wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
-sudo apt remove neovim* --purge
-sudo apt install -y ./nvim-linux64.deb
-
-# add zsh as a login shell
-command -v zsh | sudo tee -a /etc/shells
-
-# use zsh as default shell
-sudo chsh -s $(which zsh) $USER
+# install dwm
+sudo apt install -y \
+    xorg-dev \
+    libx11-dev \
+    libxinerama-dev \
+    libxft-dev
+mkdir -p $HOME/.config/suckless
+cd $HOME/.config/suckless
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dwm
+cd dmenu
+sudo make clean install
+cd $HOME/.config/suckless/dwm
+sudo make clean install
 
 # stow dotfiles
 cd $HOME/.newborn
@@ -81,6 +73,28 @@ stow zsh
 stow nvim
 stow git
 stow tmux
+stow x
+
+# install conda
+cd $HOME
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh
+
+# install ros-humble, comment next block if ros not wanted
+sudo apt install -y gnupg lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update
+sudo apt install -y ros-humble-desktop python3-colcon-common-extensions
+
+# install nvidia driver 
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
+sudo apt update
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
+sudo apt install -y ./nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
 
 # install nerd-fonts
 mkdir -p $HOME/.local/share/fonts/NerdFonts/
@@ -94,10 +108,9 @@ curl -fLo "Hack Italic Nerd Font Complete Mono.ttf" https://github.com/ryanoasis
 curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
 curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 
-# install conda
-cd $HOME
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash ./Miniconda3-latest-Linux-x86_64.sh
+# add zsh as a login shell
+command -v zsh | sudo tee -a /etc/shells
+sudo chsh -s $(which zsh) $USER # use zsh as default shell
 
 # restart computer
 echo "you may now restart your computer"
