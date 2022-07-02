f086ea6c65be2cac2895d6a238739f8742ed4bfe
Lin Zhang <linzhank@gmail.com>
2022-07-02 15:34:29 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,6 @@ sudo apt install -y \
     python3-dev \
     python3-pip \
     neofetch \
-    awesome
 
 # install pyenv
 sudo apt install -y \
@@ -38,6 +37,7 @@ sudo apt install -y \
 curl https://pyenv.run | bash
 
 # install nvim
+cd $HOME
 wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
 sudo apt remove neovim* --purge
 sudo apt install -y ./nvim-linux64.deb
@@ -86,10 +86,9 @@ sudo apt update
 sudo apt install -y ros-humble-ros-base python3-colcon-common-extensions
 
 # install nvidia driver 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
+cd $HOME
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt update
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
 sudo apt install -y ./nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
