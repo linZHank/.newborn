035a9d3236099622a5d213783631f9371b4edade
Lin Zhang <linzhank@gmail.com>
2023-01-22 12:58:53 -0600

    update keymaps and options

--- nvim/.config/nvim/lua/pbd0/keymaps.lua
+++ nvim/.config/nvim/lua/pbd0/keymaps.lua
@@ -19,7 +19,10 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Normal --
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
