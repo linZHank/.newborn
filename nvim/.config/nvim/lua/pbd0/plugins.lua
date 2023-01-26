15406143d3397d1ef5d7329d9b7308c618310ff6
Lin Zhang <linzhank@gmail.com>
2023-01-25 23:14:02 -0600

    update comment config and plugins

--- nvim/.config/nvim/lua/pbd0/plugins.lua
+++ nvim/.config/nvim/lua/pbd0/plugins.lua
@@ -62,7 +62,6 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use { "shaunsingh/nord.nvim", commit="9824b8511dcb7d89de628d7e9bab5fa65c9d59d1" }
-  use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
