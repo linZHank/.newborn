a3280aa6a8e8a39e01a2e46b733d7d2b756d136a
linzhank <linzhank@gmail.com>
2023-06-15 16:12:24 -0500

    update configs

--- setup.sh
+++ setup.sh
@@ -8,38 +8,63 @@ sudo apt install -y \
     unzip \
     make \
     build-essential \
+    python3-dev \
+    python3-pip \
     alacritty \
     zsh \
     git \
     tmux \
     stow \
-    python3-dev \
-    python3-pip \
     neofetch \
 
-# install pyenv
-sudo apt install -y \
-    libssl-dev \
-    zlib1g-dev \
-    libbz2-dev \
-    libreadline-dev \
-    libsqlite3-dev \
-    wget \
-    llvm \
-    libncursesw5-dev \
-    xz-utils \
-    tk-dev \
-    libxml2-dev \
-    libxmlsec1-dev \
-    libffi-dev \
-    liblzma-dev
-curl https://pyenv.run | bash
 
-# install nvim
+# install pyenv
+# sudo apt install -y \
+#     libssl-dev \
+#     zlib1g-dev \
+#     libbz2-dev \
+#     libreadline-dev \
+#     libsqlite3-dev \
+#     wget \
+#     llvm \
+#     libncursesw5-dev \
+#     xz-utils \
+#     tk-dev \
+#     libxml2-dev \
+#     libxmlsec1-dev \
+#     libffi-dev \
+#     liblzma-dev
+# 
+# curl https://pyenv.run | bash
+
+
+# Install neovim
+sudo apt install -y \
+    ninja-build \
+    gettext \
+    libtool \
+    libtool-bin \
+    autoconf \
+    automake \
+    cmake \
+    g++ \
+    pkg-config \
+    unzip \
+    curl \
+    doxygen \
+    lua5.4 \
+    luajit \
+    
 cd $HOME
-wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
-sudo apt remove neovim* --purge
-sudo apt install -y ./nvim-linux64.deb
+git clone https://github.com/neovim/neovim.git
+cd neovim
+git checkout v0.8.2
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local/neovim"
+make install 
+export PATH="$HOME/.local/neovim/bin:$PATH"
+cd $HOME
+echo "end install neovim..."
+
 
 # install node version manager
 curl -fsSL https://fnm.vercel.app/install | bash
@@ -47,26 +72,12 @@ export PATH=$HOME/.fnm:$PATH
 eval "$(fnm env --use-on-cd)"
 fnm install v16.14.0
 
-# install linting, formatting tools
-pip install pip pyright flake8 black --upgrade # python
-sudo apt install -y libxml2-utils tidy  # xml
-npm install -g markdownlint-cli  # markdown
 
-# install dwm
-# sudo apt install -y \
-#     xorg-dev \
-#     xinit \
-#     libx11-dev \
-#     libxinerama-dev \
-#     libxft-dev
-# mkdir -p $HOME/.config/suckless
-# cd $HOME/.config/suckless
-# git clone https://git.suckless.org/dwm
-# git clone https://git.suckless.org/dwm
-# cd dmenu
-# sudo make clean install
-# cd $HOME/.config/suckless/dwm
-# sudo make clean install
+# install linting, formatting tools
+# pip install pip pyright flake8 black --upgrade # python
+# sudo apt install -y libxml2-utils tidy  # xml
+# npm install -g markdownlint-cli  # markdown
+
 
 # stow dotfiles
 cd $HOME/.newborn
@@ -77,12 +88,14 @@ stow git
 stow tmux
 # stow x
 
+
 # install ros-humble, comment next block if ros not wanted
 sudo apt install -y gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt install -y ros-humble-ros-base python3-colcon-common-extensions
+
 
 # install nvidia driver 
 cd $HOME
@@ -91,6 +104,7 @@ sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt update
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
 sudo apt install -y ./nvidia-driver-515_515.48.07-0ubuntu1_amd64.deb
+
 
 # install nerd-fonts
 mkdir -p $HOME/.local/share/fonts/NerdFonts/
@@ -104,14 +118,18 @@ curl -fLo "Hack Italic Nerd Font Complete Mono.ttf" https://github.com/ryanoasis
 curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
 curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 
-# install conda
+
+# install mamba-forge
 cd $HOME
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash ./Miniconda3-latest-Linux-x86_64.sh
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+bash Mambaforge-$(uname)-$(uname -m).sh
+rm -f Mambaforge-$(uname)-$(uname -m).sh
+
 
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER # use zsh as default shell
+
 
 # restart computer
 echo "you may now restart your computer"
