b079096dc4b243500d479ff94503ebc2a53c6fbe
linZHank <linzhank@gmail.com>
2023-12-29 09:17:24 -0600

    Add files via upload

--- nvim/.config/nvim/lua/pbd0/keymaps.lua
+++ nvim/.config/nvim/lua/pbd0/keymaps.lua
@@ -14,6 +14,10 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<C-tab>", "<c-6>", opts)
 
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
