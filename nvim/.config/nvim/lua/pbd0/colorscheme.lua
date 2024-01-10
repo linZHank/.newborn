64415d7f5919ddb9a00fa9bc4ae0ec70aeccc8dc
linzhanguca <lzhang12@uca.edu>
2024-01-10 11:21:10 -0600

    update nvim config

--- nvim/.config/nvim/lua/pbd0/colorscheme.lua
+++ nvim/.config/nvim/lua/pbd0/colorscheme.lua
@@ -1,8 +1,8 @@
 local M = {
   "shaunsingh/nord.nvim",
-  commit = "fab04b2dd4b64f4b1763b9250a8824d0b5194b8f",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
+  commit = "80c1e5321505aeb22b7a9f23eb82f1e193c12470",
 }
 
 M.name = "nord"
---
+++ nvim/.config/nvim/lua/pbd0/keymaps.lua
@@ -0,0 +1,41 @@
+local keymap = vim.keymap.set  -- shorten function name
+local opts = { noremap = true, silent = true }  
+
+keymap("n", "<Space>", "", opts)
+vim.g.mapleader = " "  -- remap space as leader key
+vim.g.maplocalleader = " "
+
+keymap("n", "<C-i>", "<C-i>", opts)
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-tab>", "<c-6>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+keymap("x", "p", [["_dP]])  -- not sure
+
+vim.cmd [[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definition()<CR>]]
+vim.cmd [[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
+-- vim.cmd [[:amenu 10.120 mousemenu.-sep- *]]
+
+vim.keymap.set("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
+vim.keymap.set("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
+
+-- NvimTree
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
