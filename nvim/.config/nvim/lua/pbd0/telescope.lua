7e6fb6181edbde75bd3841f308b1f5a223d62cd1
Lin Zhang <linzhank@gmail.com>
2024-05-16 22:25:56 -0500

    register telescope keys to which-key

--- nvim/.config/nvim/lua/pbd0/telescope.lua
+++ nvim/.config/nvim/lua/pbd0/telescope.lua
@@ -1,14 +1,58 @@
 local M = {
   "nvim-telescope/telescope.nvim",
-  dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true } },
-  commit = "74ce793a60759e3db0d265174f137fb627430355",
-  lazy = true,
-  cmd = "Telescope",
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        }
+    },
+    lazy = true,
+    cmd = "Telescope",
 }
 
 function M.config()
-  local icons = require "pbd0.icons"
-  local actions = require "telescope.actions"
+    local wk = require "which-key"
+    wk.register {
+        ["<leader>bb"] = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+        ["<leader>fb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+        ["<leader>fc"] = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+        ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find files" },
+        ["<leader>fp"] = { 
+            "<cmd>lua require('telescope').extensions.projects.projects()<cr>",
+            "Projects",
+        },
+        ["<leader>ft"] = { "<cmd>Telescope live_grep<cr>", "Find Text" },
+        ["<leader>fs"] = { "<cmd>Telescope grep_string<cr>", "Find String" },
+        ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help" },
+        ["<leader>fH"] = { "<cmd>Telescope highlights<cr>", "Highlights" },
+        ["<leader>fi"] = {
+            "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", 
+            "Media",
+        },
+        ["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
+        ["<leader>fM"] = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+        ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
+        ["<leader>fR"] = { "<cmd>Telescope registers<cr>", "Registers" },
+        ["<leader>fk"] = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+        ["<leader>fC"] = { "<cmd>Telescope commands<cr>", "Commands" },
+        ["<leader>go"] = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+        ["<leader>gb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+        ["<leader>gc"] = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+        ["<leader>gC"] = {"<cmd>Telescope git_bcommits<cr>", "Checkout commit(for current file)"},
+        ["<leader>ls"] = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+        ["<leader>lS"] = {
+          "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+          "Workspace Symbols",
+        },
+        ["<leader>le"] = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+    }
+    local icons = require "pbd0.icons"
+    local actions = require "telescope.actions"
 
   require("telescope").setup {
     defaults = {
