1e20a9d38561f943e28cbfe925e2bfd7eb689ed2
linzhank <linzhank@gmail.com>
2024-07-04 13:15:56 -0500

    update autocompletion

--- nvim/.config/nvim/lua/plugin_configs/autocompletion.lua
+++ nvim/.config/nvim/lua/plugin_configs/autocompletion.lua
@@ -56,8 +56,8 @@ return {
                 mapping = cmp.mapping.preset.insert {
                     -- Select the [n]ext item
                     ['<C-n>'] = cmp.mapping.select_next_item(),
-                    -- Select the [N]previous item
-                    ['<C-N>'] = cmp.mapping.select_prev_item(),
+                    -- Select the [P]revious item
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
 
                     -- Scroll the documentation window [U]p / [D]own
                     ['<C-U>'] = cmp.mapping.scroll_docs(-4),
@@ -66,7 +66,7 @@ return {
                     -- Accept ([y]es) the completion.
                     --  This will auto-import if your LSP supports it.
                     --  This will expand snippets if the LSP sent a snippet.
-                    -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+                    ['<C-y>'] = cmp.mapping.confirm { select = true },
 
                     -- If you prefer more traditional completion keymaps,
                     -- you can uncomment the following lines
