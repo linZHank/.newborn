1df20cdd6aa360f67cdf577faa41fa7e1e0ea753
linzhank <linzhank@gmail.com>
2024-08-08 12:27:22 -0500

    update nvim setup
    with dependencies installation
    - lazygit
    - ripgrep

--- setup.sh
+++ setup.sh
@@ -3,65 +3,62 @@ cd $HOME
 sudo dnf update
 sudo dnf group install 'Hardware Support'
 
-
-# import RPM Fusion 
+# import RPM Fusion
 sudo dnf install -y \
-https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y \
-https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf upgrade --refresh
-
 
 # install packages
 sudo dnf install -y \
-    @development-tools \
-    curl \
-    alacritty \
-    zsh \
-    git \
-    tmux \
-    stow \
-    neofetch \
-    htop \
+  @development-tools \
+  curl \
+  alacritty \
+  zsh \
+  git \
+  tmux \
+  stow \
+  neofetch \
+  htop
 
+sudo dnf copr enable atim/lazygit -y
+sudo dnf install -y \
+  ninja-build \
+  libtool \
+  autoconf \
+  automake \
+  cmake \
+  gcc \
+  gcc-c++ \
+  make \
+  pkgconfig \
+  unzip \
+  patch \
+  gettext \
+  curl \
+  xclip \
+  xsel \
+  wl-clipboard \
+  ripgrep \
+  lazygit
 
 # install neovim
-echo "start install neovim..."
-cd $HOME
-sudo dnf install -y \
-    ninja-build \
-    libtool \
-    autoconf \
-    automake \
-    cmake \
-    gcc \
-    gcc-c++ \
-    make \
-    pkgconfig \
-    unzip \
-    patch \
-    gettext \
-    curl \
-    xclip \
-    xsel \
-    wl-clipboard
-    
+echo "Start install neovim..."
 cd $HOME
 git clone https://github.com/neovim/neovim.git
 cd neovim
-git checkout v0.9.4
-make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/.local/neovim
-make install 
+git checkout stable
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local/neovim
+make install
 cd $HOME
 echo "End build neovim."
-
 
 # install node version manager
 curl -fsSL https://fnm.vercel.app/install | bash
 export PATH=$HOME/.local/share/fnm:$PATH
 eval "$(fnm env --use-on-cd)"
 fnm install v16.14.0
-
 
 # install conda
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
@@ -70,7 +67,6 @@ rm -f Mambaforge-$(uname)-$(uname -m).sh
 
 # install tmux package manager
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-
 
 # stow dotfiles
 cd $HOME/.newborn
@@ -81,16 +77,13 @@ stow git
 stow tmux
 stow git
 
-
 # use zsh as default shell
 command -v zsh | sudo tee -a /etc/shells # set zsh as login shell
 sudo dnf install -y util-linux-user
 sudo chsh -s $(which zsh) $USER
 
-
 # install nvidia driver
 sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
-
 
 # install nerd-fonts
 mkdir -p $HOME/.local/share/fonts/NerdFonts/
@@ -101,7 +94,6 @@ curl -fLo "BigBlueTermPlus Nerd Font Propo.ttf" https://github.com/ryanoasis/ner
 fc-cache -f -v
 cd $HOME
 echo "Fonts downloaded."
-
 
 # restart computer
 echo "you may now restart your computer"
