b294d99bf1586837b64b7cdee07508558ae9c97f
linzhanguca <lzhang12@ucae.edu>
2025-02-06 17:18:08 -0600

    update lsp config

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -1,37 +1,78 @@
 return {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-    build = ":MasonUpdate",
-    opts_extend = { "ensure_installed" },
-    opts = {
-        ensure_installed = {
-            "stylua",
-            "marksman",
-            "pyright",
+    {
+        "neovim/nvim-lspconfig",
+        event = "VeryLazy",
+        dependencies = {
+            "mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
         },
+        opts = {
+        },
+        config = function()
+            require("lspconfig").lua_ls.setup({
+                settings = {
+                    Lua = {
+                        workspace = {
+                            checkThirdParty = false,
+                        },
+                        codeLens = {
+                            enable = true,
+                        },
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        doc = {
+                            privateName = { "^_" },
+                        },
+                        hint = {
+                            enable = true,
+                            setType = false,
+                            paramType = true,
+                            paramName = "Disable",
+                            semicolon = "Disable",
+                            arrayIndex = "Disable",
+                        },
+                    },
+                }
+            })
+        end
     },
-    ---@param opts MasonSettings | {ensure_installed: string[]}
-    config = function(_, opts)
-        require("mason").setup(opts)
-        local mr = require("mason-registry")
-        mr:on("package:install:success", function()
-            vim.defer_fn(function()
-                -- trigger FileType event to possibly load this newly installed LSP server
-                require("lazy.core.handler.event").trigger({
-                    event = "FileType",
-                    buf = vim.api.nvim_get_current_buf(),
-                })
-            end, 100)
-        end)
 
-        mr.refresh(function()
-            for _, tool in ipairs(opts.ensure_installed) do
-                local p = mr.get_package(tool)
-                if not p:is_installed() then
-                    p:install()
+    {
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+        build = ":MasonUpdate",
+        opts_extend = { "ensure_installed" },
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "marksman",
+                "pyright",
+            },
+        },
+        ---@param opts MasonSettings | {ensure_installed: string[]}
+        config = function(_, opts)
+            require("mason").setup(opts)
+            local mr = require("mason-registry")
+            mr:on("package:install:success", function()
+                vim.defer_fn(function()
+                    -- trigger FileType event to possibly load this newly installed LSP server
+                    require("lazy.core.handler.event").trigger({
+                        event = "FileType",
+                        buf = vim.api.nvim_get_current_buf(),
+                    })
+                end, 100)
+            end)
+
+            mr.refresh(function()
+                for _, tool in ipairs(opts.ensure_installed) do
+                    local p = mr.get_package(tool)
+                    if not p:is_installed() then
+                        p:install()
+                    end
                 end
-            end
-        end)
-    end,
+            end)
+        end,
+    },
 }
